a92735f54ce5a01f0f80f57c64280d0a8f05132c
Sanguinou <mattbiasse@gmail.com>
2021-06-23 19:03:07 +0200

    add orders.vue + ordercard component

---
+++ components/OrderCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex justify-center my-4">
+    <div class="grid grid-cols-3 justify-items-center items-center w-5/6 h-32">
+      <div class="bg-gray-200 rounded w-4/5 h-full"></div>
+      <div class="justify-self-start">
+        <p class="font-bold text-xl">O'Tacos - Arras</p>
+        <p class="text-gray-500 my-4">5 items • 39.17€</p>
+        <p class="text-gray-500">Jun 13 • Completed</p>
+      </div>
+      <button
+        class="
+          bg-gray-400
+          text-white
+          font-bold
+          justify-center
+          rounded-full
+          w-24
+          h-8
+        "
+      >
+        Reorder
+      </button>
+    </div>
+  </div>
+</template>
--- pages/Orders.vue
+++ pages/Orders.vue
@@ -1,3 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <div class="shadow-md flex justify-center items-center w-full h-28">
+      <div class="font-bold text-3xl tracking-widest">ORDERS</div>
+    </div>
+    <OrderCard />
+    <div class="flex bg-gray-200 m-auto mt-10 w-4/5 h-1"></div>
+    <OrderCard />
+  </div>
 </template>
+
+<script>
+import OrderCard from '@/components/OrderCard'
+
+export default {
+  components: {
+    OrderCard,
+  },
+}
+</script>
